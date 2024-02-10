1a2259f59c7549effe9b639171ae82a13b79dee6
yanabg <36585715+yanabg@users.noreply.github.com>
2024-02-10 23:41:36 +0200

    Update storage.tf
    azurerm_storage_account -> from
    azurerm_resource_group.rgs.location to var.resource_group_location for the location

--- storage.tf
+++ storage.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rgs" {
 resource "azurerm_storage_account" "storageaccount" {
   name                     = "taskboardstorageybg"
   resource_group_name      = azurerm_resource_group.rgs.name
-  location                 = azurerm_resource_group.rgs.location
+  location                 = var.resource_group_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
