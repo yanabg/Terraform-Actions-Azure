afc73dd5171bcf573f38be0d00543b90d97daee6
yanabg <36585715+yanabg@users.noreply.github.com>
2024-02-10 23:12:24 +0200

    Update storage.tf
    deleted file content

--- storage.tf
+++ storage.tf
@@ -1,18 +1,1 @@
-resource "azurerm_resource_group" "rgs" {
-  name     = "StorageRG"
-  location = "North Europe"
-}
 
-resource "azurerm_storage_account" "storageaccount" {
-  name                     = "taskboardstorageybg"
-  resource_group_name      = azurerm_resource_group.rgs.name
-  location                 = azurerm_resource_group.rgs.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "container" {
-  name                  = "taskboardcontainer"
-  storage_account_name  = azurerm_storage_account.storageaccount.name
-  container_access_type = "private"
-}
