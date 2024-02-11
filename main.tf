cc0d630bd43fa82c3155391a557ae9f3ff4d4bc4
yanabg <36585715+yanabg@users.noreply.github.com>
2024-02-11 14:13:23 +0200

    Update main.tf
    defined storage resource group as valu and not as variable

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
 backend "azurerm" {
-  resource_group_name   = var.storage_resource_group_name
+  resource_group_name   = "StorageRG"
   storage_account_name  = "taskboardstorageybg"
   container_name        = "taskboardcontainer"
   key                   = "taskboard.tfstate"
