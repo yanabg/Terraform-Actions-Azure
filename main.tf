9e8be88fa1dc5c9426dbb5ec7fec9fd5904aba10
yanabg <36585715+yanabg@users.noreply.github.com>
2024-02-11 13:58:21 +0200

    Update main.tf
    add service principal part to main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,10 @@ backend "azurerm" {
 
 provider "azurerm" {
   features {}
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  tenant_id       = var.ARM_TENANT_ID
 }
 
 resource "random_integer" "ri" {
